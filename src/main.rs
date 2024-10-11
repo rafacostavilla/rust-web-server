8e4d560f29bf428e9e4d0d5c46556e00f84f502e
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-11 18:22:05 +1300

    Hand-code a default success response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,5 +19,7 @@ fn handle_connection(mut stream: TcpStream){
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    
+    stream.write_all(response.as_bytes()).unwrap();
 }
