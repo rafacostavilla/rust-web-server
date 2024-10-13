f1121fc7c602600bd11dd6f27259e12445f75f2a
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-13 20:12:05 +1300

    Implement the Drop Trait for the ThreadPool and signal to the threads to stop listening fot jobs (limit the number of streams to 2 in main to check the dropping behaviour

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 pub struct ThreadPool{
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
     workers: Vec<Worker>,
 }
 
@@ -27,7 +27,10 @@ impl ThreadPool{
             workers.push(Worker::new(id, Arc::clone(&receiver)));            
         }
         
-        ThreadPool{sender, workers}
+        ThreadPool{
+            sender: Some(sender), 
+            workers,
+        }
     }
     
     pub fn execute<F>(&self, f:F)
@@ -36,31 +39,58 @@ impl ThreadPool{
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender
+            .as_ref()
+            .take()
+            .unwrap()
+            .send(job)
+            .unwrap();
     }
+}
+
+impl Drop for ThreadPool{
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers{
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap()
+            }
+        }
+    }
+
 }
 struct Worker{
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker{
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>)->Worker{
         let thread = thread::spawn(move || loop {
-            let job = receiver
+            let message = receiver
                 .lock()
                 .unwrap()
-                .recv()
-                .unwrap();
+                .recv();
 
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                    println!("Worker {id} finished a job; completed.");
+                },
+                _ =>{
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
         Worker{
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,27 @@ fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming().take(2){
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 // This function consumes stream
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&mut stream);
-
     let request_line = buf_reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap();
-    
+    .lines()
+    .next()
+    .unwrap()
+    .unwrap();
+
+println!("Finished request_line");
     let (status_line,filepath) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK","hello.html"),
         "GET /sleep HTTP/1.1" => {
